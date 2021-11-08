4aeebb7fe681e2a6fd93940f067a6e7674cb4cd2
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2021-11-08 02:10:10 -0700

    Added another system runtime for program runtime

--- main.java
+++ main.java
@@ -931,42 +931,9 @@ public class main {
 		    this.setSize(400, 180);
 		}
 	}
-	public static class runtime{
-		private static int i = 0;
-		public static int seconds = 0;
-		public static int minutes = 0;
-		public static int hours = 0;
-	    public static void run()
-	    {
-	        ++seconds;
-	        if(seconds == 60) {
-	        	seconds = 0;
-	        	minutes = minutes + 1;
-	        }
-	        if(minutes == 60) {
-	        	minutes = 0;
-	        	hours = hours + 1;
-	        }
-	    }
-	    public static void display(JLabel label) {
-	    	String second = String.valueOf(seconds);
-	    	String minute = String.valueOf(minutes);
-	    	String hour = String.valueOf(hours);
-	    	if(seconds <=9) {
-	    		second = String.format("%02d", seconds);
-	    	}
-	    	if(minutes <=9) {
-	    		minute = String.format("%02d", minutes);
-	    	}
-	    	if(hours <=9) {
-	    		hour = String.format("%02d", hours);
-	    	}
-	    	String display = hour + ":" + minute + ":" + second;
-	    	label.setText(display);
-	    }
-	}
 	public static class runtimeClock extends JPanel{
 		public runtimeClock() {
+			Language.Sys.runtime runtime = new Language.Sys.runtime();
 			JLabel timeLable = new Components.Label("Time: ", Colors.colors.darkpurple);
 			JLabel time = new Components.Label("", Colors.colors.purple);
 			super.add(timeLable);
